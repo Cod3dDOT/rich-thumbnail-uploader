a8f4577891cff4ad297d844ca6ea15261c560455
cod3d <64918545+Cod3dDOT@users.noreply.github.com>
2025-07-18 18:04:03 -0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -21,7 +21,7 @@ fn main() {
     let app_name = std::env::var("CARGO_PKG_NAME").unwrap();
 
     let manifest = embed_manifest::new_manifest(app_name.as_str())
-        .requested_execution_level(embed_manifest::manifest::ExecutionLevel::AsInvoker)
+        // .requested_execution_level(embed_manifest::manifest::ExecutionLevel::AsInvoker)
         .max_version_tested(embed_manifest::manifest::MaxVersionTested::Windows11)
         .to_string();
 
